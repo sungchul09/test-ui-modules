e0d7dbc2644c824ccd149a743bf40040e53c4970
sungchul09 <sungchul09@naver.com>
2023-09-20 01:46:36 +0900

    feat: 헤더 추가 - 페이지 헤드 추가 (components/common)

---
+++ components/common/TheHeader.vue
@@ -0,0 +1,47 @@
+<template>
+  <header class="header">
+    <h2 class="header-text">🎨 scyun 컴포넌트</h2>
+    <div class="header-menu">
+      <nuxt-link :to="{ name: 'card' }">card 컴포넌트</nuxt-link>
+      <nuxt-link :to="{ name: 'input' }">input 컴포넌트</nuxt-link>
+    </div>
+  </header>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+.header {
+  @include font_2060;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  padding: 20px 40px;
+  gap: 20px;
+  position: relative;
+  margin-bottom: 50px;
+}
+.header::after {
+  position: absolute;
+  content: '';
+  border: 1px solid $systemBorderColor;
+  width: 100%;
+  border-top: 0px;
+  left: 0;
+  bottom: 0;
+}
+.header-menu {
+  @include font_2060;
+  display: flex;
+  justify-content: flex-start;
+  gap: 20px;
+  & > a {
+    &:visited {
+      color: inherit;
+    }
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,21 @@
 <template>
   <div>
+    <TheHeader />
     <slot />
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { CommonTheHeader as TheHeader } from '#components';
+definePageMeta({
+  components: {
+    TheHeader,
+  },
+});
+</script>
 
-<style scoped></style>
+<style scoped>
+div {
+  height: 100vh;
+}
+</style>
